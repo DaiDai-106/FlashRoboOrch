bf39761c528152cc984a110a6b0c63f4852ff99f
SmileSmile_ <yifan_wang@roboticplus.com>
2026-01-04 13:33:57 +0800

    update:初步完成简单环境配置,并引入了新定义的数据结构的包

---
+++ main.py
@@ -0,0 +1,22 @@
+from data_model import (
+    ObjectAction,
+    RobotScene,
+    RobotInfo,
+    ObjectInfo,
+    Transform,
+    TransformOnFrame,
+    RobotFrame,
+    RobotState,
+    Trajectory,
+    RobotFrameSequence,
+)
+
+
+
+
+def main():
+    print("Hello from flashroboorch!")
+
+
+if __name__ == "__main__":
+    main()
