84fc3658b3208b7bc1fc78793a93b3611381b7dd
SmileSmile_ <yifan_wang@roboticplus.com>
2026-01-05 15:38:54 +0800

    update:初步完成了使用viser对简单机械臂URDF的场景搭建

--- src/robots_orchestra/__init__.py
+++ src/robots_orchestra/__init__.py
@@ -1,2 +1,8 @@
 """Robots Orchestra - 机器人编排系统"""
+from pathlib import Path
 
+# 声明目前的包中囊括的URDF及其对应的几何内容
+PROJECT_ROOT = Path(__file__).parent
+SCENE_DIR = PROJECT_ROOT / "scene"
+
+__all__ = ["SCENE_DIR", "PROJECT_ROOT"]
--- src/robots_orchestra/controller/usr_session.py
+++ src/robots_orchestra/controller/usr_session.py
@@ -1,5 +1,6 @@
 import viser
 import time
+import numpy as np
 from typing import Dict
 from robots_orchestra.viz.viser import ViserUI
 from viser.extras import ViserUrdf
@@ -11,26 +12,20 @@ class UserSession:
     def __init__(self, client: viser.ClientHandle, viser_ui: ViserUI):
         self.ui = viser_ui
         self.client = client
-        self.robots: Dict[str, Dict[str, Any]] = {}  
-        
+        self.robots: Dict[str, Any] = {}  
 
     def add_urdf(self, urdf: URDF):
-        name = urdf.name
-        base_node_name = f"/user_{self.client.client_id}/base_link_{name}"
+        name = urdf.robot.name
         if name in self.robots:
             self.remove_urdf(name)
 
-        frame_handle = self.ui.server.scene.add_frame(base_node_name, show_axes=False)
-        viser_urdf_handle = ViserUrdf(self.ui.server, urdf, root_node_name=f"/base_link_{name}")
-        self.robots[name] = {
-            "frame": frame_handle,
-            "visualizer": viser_urdf_handle
-        }
-
+        viser_urdf_handle = ViserUrdf(self.ui.server, urdf, root_node_name=f"/world/origin/base_link_{name}", load_collision_meshes = False) 
+        self.robots[name] = viser_urdf_handle
+        actuated_joints = urdf.actuated_joints
+        dof = len(actuated_joints)  # 获取当前机械臂的自由度 
+        default_joint = np.array([0.001] * dof, dtype=np.float64)
+        self.robots[name].update_cfg(default_joint)
 
     def remove_urdf(self, name: str) -> None:
         if name in self.robots:
-            record = self.robots[name]
-            record["frame"].remove()
-            record["visualizer"].remove()
             del self.robots[name]
